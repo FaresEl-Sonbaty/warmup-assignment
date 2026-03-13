512ebc74c4b5ba84f398479dbf5e04b6a5171ddc
FaresEl-Sonbaty <fares.elsonbaty@student.giu-uni.de>
2026-03-13 17:12:10 +0200

    Leading Zeroes Error removed from Hours, All Tests Passed

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function secondsToTime(totalSeconds) {
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
     
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${hours.toString()}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 //2- time converters to and from seconds format without am/pm
@@ -33,7 +33,7 @@ function secondsToHours(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+    return `${hours.toString()}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
 
 
